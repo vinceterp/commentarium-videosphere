17353ad24fbff617a8045840b6101177566836b6
Paul Campbell <vinceterp@outlook.com>
2025-07-17 22:05:06 -0400

    verification cleanup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -60,9 +60,10 @@ const Navbar = () => {
       style={{ borderWidth: 0 }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <Link to="/" className="text-2xl font-bold">
-            Commentarium
+        <div className="flex flex-row items-center justify-between h-16">
+          <Link to="/" className="text-2xl font-bold flex items-center">
+            <p>Commentarium</p>
+            <img src={"/logo.svg"} alt="Logo" className="h-6 w-6 ml-2" />
           </Link>
 
           <div className="flex items-center gap-4">
--- src/stores/users.ts
+++ src/stores/users.ts
@@ -12,6 +12,7 @@ export interface User {
   token: string;
   refreshToken: string;
   userId?: number;
+  isEmailVerified?: boolean;
 }
 
 export interface UserStore {
